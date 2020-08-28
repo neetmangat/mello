8e76b377a5d77a6957b1080f9f08fdfa45dffa2d
neetmangat <neetmangat@gmail.com>
2020-08-28 23:58:42 +0000

    22. Creating and Rendering Lists

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -1,5 +1,8 @@
 const $logoutButton = $('#logout');
 const $boardContainer = $('.container');
+const $boardName = $('header > h1');
+const $createListInput = $('#create-list input');
+const $saveListButton = $('#create-list .save');
 
 let board;
 
@@ -24,16 +27,6 @@ function getBoard(id) {
         });
 }
 
-function handleLogout() {
-    $.ajax({
-        url: '/logout',
-        method: 'DELETE'
-    }).then(function() {
-        localStorage.clear();
-        location.replace('/');
-    });
-}
-
 function createLists(lists) {
     let $listContainers = lists.map(function(list) {
         let $listContainer = $('<div class="list">');
@@ -48,14 +41,64 @@ function createLists(lists) {
         return $listContainer;
     });
 
-    return $listContainers
+    let $addListContainer = $('<div class="list add">');
+    let $addListButton = $('<button>')
+        .text('+ Add another list')
+        .on('click', openListCreateModal);
+
+    $addListContainer.append($addListButton);
+    $listContainers.push($addListContainer);
+
+    return $listContainers;
 }
 
 function renderBoard() {
     let $lists = createLists(board.lists);
+    $boardName.text(board.name);
 
     $boardContainer.empty();
     $boardContainer.append($lists);
 }
 
+function openListCreateModal() {
+    $createListInput.val('');
+    MicroModal.show('create-list');
+}
+
+function handleListCreate(event) {
+    event.preventDefault();
+
+    let listTitle = $createListInput.val().trim();
+
+    $createListInput.val('');
+
+    if (!listTitle) {
+        MicroModal.close('create-list');
+        return;
+    }
+
+    $.ajax({
+        url: '/api/lists',
+        method: 'POST',
+        data: {
+            board_id: board.id,
+            title: listTitle
+        }
+    }).then(function() {
+        init();
+        MicroModal.close('create-list');
+    })
+}
+
+function handleLogout() {
+    $.ajax({
+        url: '/logout',
+        method: 'DELETE'
+    }).then(function() {
+        localStorage.clear();
+        location.replace('/');
+    });
+}
+
+$saveListButton.on('click', handleListCreate);
 $logoutButton.on('click', handleLogout);
